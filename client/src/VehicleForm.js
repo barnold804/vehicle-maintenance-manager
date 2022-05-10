ea5bc6b9e85194cfcb6dd906c572ece77005c649
Brian Arnold <b.arnold09@gmail.com>
2022-05-10 19:16:01 -0400

    Changes add vehicle form to select-forms, includes Year and Make but is otherwise incomplete

--- client/src/VehicleForm.js
+++ client/src/VehicleForm.js
@@ -1,28 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Select from 'react-select';
-import axios from 'axios';
 
-function VehicleForm({ user, vehicles, setVehicles, setCurrentVehicle }) {
+function VehicleForm({user, vehicles, setVehicles, setCurrentVehicle}) {
+  function range(start, end) {
+    return Array.from(Array(end - start + 2).keys()).map((v) => v + start)
+  }
 
+  const CURRENT_YEAR = new Date().getFullYear();
+  const AUTOMOBILE_YEARS = range(1888, CURRENT_YEAR)
+    .map((year) => {
+      return {value: year, label: year}
+    })
+
+  // Years!
+  const [selectedYear, setSelectedYear] = useState("")
+  const [searchYear, setSearchYear] = useState("")
+  // Might not need?
   const [year, setYear] = useState("")
+
+  // Makes!
+  const [allVehicleMakes, setAllVehicleMakes] = useState([])
+  const [filteredVehicleMakes, setFilteredVehicleMakes] = useState([])
+  const [searchMake, setSearchMake] = useState("")
+  const [selectedMake, setSelectedMake] = useState("")
+  // Might not need?
   const [make, setMake] = useState("")
+  // TBD
   const [model, setModel] = useState("")
   const [mileage, setMileage] = useState("")
   const [errors, setErrors] = useState([]);
-  const [vehicleMakeList, setVehicleMakeList] = useState([]);
-
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-  ]
 
   function clearForm() {
-      setYear("")
-      setMake("")
-      setModel("")
-      setMileage("")
-      setErrors([])
+    setYear("")
+    setMake("")
+    setModel("")
+    setMileage("")
+    setErrors([])
   }
 
   function handleCreateVehicle(e) {
@@ -33,7 +46,7 @@ function VehicleForm({ user, vehicles, setVehicles, setCurrentVehicle }) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        year,
+        year,   // this may break
         make,
         model,
         mileage,
@@ -52,58 +65,73 @@ function VehicleForm({ user, vehicles, setVehicles, setCurrentVehicle }) {
     });
   }
 
+  function translateMakeToOption(make) {
+    return {value: make.Make_ID, label: make.Make_Name}
+  }
+
   useEffect(() => {
-    fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=json").then((r) => {
-      if (r.ok) {
-        r.json().then((responseBodyJSON) => {
-          let theResults = responseBodyJSON["Results"]
-          // console.log(theResults)
-          // console.log(responseBodyJSON)
-          let resultArray = theResults.map(vm => ({value: vm.Make_Name, label: vm.Make_Name}))
-          // console.log(resultArray)
-          setVehicleMakeList(resultArray)
-        });
-      }
-    });
+    fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=json")
+      .then(response => {
+        if(response.ok) {
+          response.json().then((json) => {
+            let results = json['Results'];
+            let makes = results.map(translateMakeToOption);
+            setAllVehicleMakes(makes)
+            setFilteredVehicleMakes(makes.slice(0, 20))
+          })  
+        } else {
+          console.log("Fetch came back with non-200 status")
+        }
+      })
   }, []);
 
+  function onMakeSearchInputChange(searchTerm) {
+    setSearchMake(searchTerm)
+    let filteredMakes = allVehicleMakes.filter(make => {
+      return make.label.toLowerCase().includes(searchTerm.toLowerCase())
+    }).slice(0, 25)
+    setFilteredVehicleMakes(filteredMakes)
+  }
+
   return (
-    <div>
-      {errors.map((err) => (
-        <h3
-          key={err}
-          style={{ display: "block", margin: "auto", marginTop: 10 }}
-        >
-          {err}
-        </h3>
-      ))}
-        <h3>Add a Vehicle:</h3>
-      <form onSubmit={handleCreateVehicle}>
-        <label>
-          Year:
-          <input type="text" name="year" value={year} onChange={(e) => setYear(e.target.value)} />
-        </label>
-        <label>
-          Make:
-          <Select 
-            value={vehicleMakeList}
-            options={vehicleMakeList}/>
-          <input type="text" name="make" value={make} onChange={(e) => setMake(e.target.value)}/>
-        </label>
-        <label>
-          Model:
-          <input type="text" name="model" value={model} onChange={(e) => setModel(e.target.value)}/>
-        </label>
-        <label>
-          Mileage:
-          <input type="text" name="mileage" pattern="[0-9]*" value={mileage} onChange={(e) =>
-            setMileage((v) => (e.target.validity.valid ? e.target.value : v))
-          } />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      {/* <button onClick={getVehicleMakeList}>Get All Vehicle Makes</button> */}
-    </div>
+    (
+      allVehicleMakes.length === 0 ?
+        <div>Loading data...</div>
+        :
+        <div>
+          {errors.map((err) => (
+            <h3
+              key={err}
+              style={{display: "block", margin: "auto", marginTop: 10}}
+            >
+              {err}
+            </h3>
+          ))}
+          <h3>Add a Vehicle:</h3>
+          <form onSubmit={handleCreateVehicle}>
+            <label>
+              Year:
+              <Select name="year"
+                      options={AUTOMOBILE_YEARS}
+                      value={selectedYear}
+                      onChange={setSelectedYear}
+                      inputValue={searchYear}
+                      onInputChange={setSearchYear}/>
+            </label>
+            <label>
+              Make:
+              <Select name="make"
+                      options={filteredVehicleMakes}
+                      value={selectedMake}
+                      onChange={setSelectedMake}
+                      inputValue={searchMake}
+                      onInputChange={onMakeSearchInputChange}
+              />
+            </label>
+            <input type="submit" value="Submit"/>
+          </form>
+        </div>
+    )
   )
 }
 
