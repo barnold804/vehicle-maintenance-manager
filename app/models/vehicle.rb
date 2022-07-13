2e701fd0cd01538e3f00792befccf46d91d6ffab
Brian Arnold <b.arnold09@gmail.com>
2022-07-12 23:51:37 -0400

    Adds functionality for vehicle mileage to update to the highest from associated maintenance records

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -5,4 +5,8 @@ class Vehicle < ApplicationRecord
     validates :make, presence: true 
     validates :model, presence: true
     validates :mileage, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+
+    def find_highest_maintenance_record_mileage
+        maintenance_records.order(mileage: :desc).first&.mileage || mileage || 0
+    end
 end
--- app/serializers/vehicle_serializer.rb
+++ app/serializers/vehicle_serializer.rb
@@ -1,3 +1,7 @@
 class VehicleSerializer < ActiveModel::Serializer
   attributes :id, :year, :make, :model, :mileage
+
+  def mileage
+    object.find_highest_maintenance_record_mileage
+  end
 end
