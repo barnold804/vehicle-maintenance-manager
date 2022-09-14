3f8e9a60a450a85186c611e3c84e89be0701da6d
Brian Arnold <b.arnold09@gmail.com>
2022-09-13 23:47:42 -0400

    Revises vehicle validations

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,10 +1,9 @@
 class Vehicle < ApplicationRecord
     belongs_to :user
     has_many :maintenance_records, dependent: :destroy
-    validates :year, presence: true, :numericality => { :greater_than_or_equal_to => 1888 }
-    validates :make, presence: true 
-    validates :model, presence: true
-    validates :mileage, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+    validates :year, :numericality => { :greater_than_or_equal_to => 1888 }
+    validates_presence_of :year, :make, :model, :mileage  
+    validates :mileage, :numericality => { :greater_than_or_equal_to => 0 }
 
     def find_highest_maintenance_record_mileage
         maintenance_records.order(mileage: :desc).first&.mileage || mileage || 0
